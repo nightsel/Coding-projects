296fb535519d7c3dc1421e1305087ccd5da79ec7
Max <max.knaerva33@gmail.com>
2025-09-22 13:12:04 +0300

    Update containsduplicateiii.cpp

--- leetcode/containsduplicateiii.cpp
+++ leetcode/containsduplicateiii.cpp
@@ -4,23 +4,27 @@ public:
 
         // 3 valuediff means, find the previous -3, -2, -1 , 0, 1, 2, 3 value distance  value from nums and check its distance. if distance lower, then a duplicate has been found.
 
-        // with this solution, problem arises when the valuediff is very large and a lot of useless values are checked instead of just checking if there's a value within that index range nearby.
+        // with this solution, problem arises when the valuediff is very large and a lot of useless values are checked instead of just checking if any of the indices in the range even have their value within valuediff.
 
-        unordered_map<int,int> lastSeen; // number → last index
+        // Switch to deque instead and store only the last x indexdiffs in the deque.
+        // deque takes too long with indexdiff being large, although works in most cases
+
+        // Switch to hash buckets (chatgpt suggestion)
+        int foundindices = 0;
+        deque<int> lastSeen;
+       // unordered_map<long, long> buckets;
         for (int i = 0; i < nums.size(); i++) {
-            for (int k = 0 ; k<= valueDiff; k++) {
-                if (lastSeen.count(nums[i]-k)) {
-                    if ( i - lastSeen[nums[i]-k] <= indexDiff) {
-                        return true;
-                    }
-                }
-                if (lastSeen.count(nums[i]+k)) {
-                    if ( i - lastSeen[nums[i]+k] <= indexDiff) {
-                        return true;
-                    }
+            for (int num : lastSeen) {
+                if (abs(num - nums[i]) <= valueDiff) {
+                    return true;
                 }
             }
-            lastSeen[nums[i]] = i;
+
+            lastSeen.push_back(nums[i]);
+
+            if (lastSeen.size() > indexDiff) {
+                lastSeen.pop_front();
+            }
         }
         return false;
     }
